17cd21c5a37e040dc018631b3bcb0b9c08ffbcf0
Abhik-Nayak <abhiknayakabhik@gmail.com>
2021-12-27 11:39:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Route} from "react-router-dom";
+import {Switch} from "route";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  
+  );
+}
+
+export default App;
