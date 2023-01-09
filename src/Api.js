6ca8e11b6f5ae83dca706d8999a8a452047145ee
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-01-09 22:21:06 +0530

    Abhik/09-01-23

--- src/Api.js
+++ src/Api.js
@@ -3,11 +3,11 @@ const api="http://localhost:5000";
 
 export const getUers=`${api}/user`;
 
-const url = "http://localhost:5000/user";
+// const url = "http://localhost:5000/user";
 
 
-export const getUse =() =>{ axios.get(url).then((res) =>{return res})};
-export const createUser =(user) =>{ axios.post(url,user).then((res) =>{return res})};
-export const deleteUser =(id) =>{ axios.delete(`${url}/${id}`).then((res) =>{return res})};
-export const getSingleuser = (id) =>{ axios.get(`${url}/${id}`).then((res) =>{return res})};
-export const updateUser = (id,user) =>{ axios.put(`${url}/${id}`,user).then((res) =>{return res})};
+// export const getUse =() =>{ axios.get(url).then((res) =>{return res})};
+// export const createUser =(user) =>{ axios.post(url,user).then((res) =>{return res})};
+// export const deleteUser =(id) =>{ axios.delete(`${url}/${id}`).then((res) =>{return res})};
+// export const getSingleuser = (id) =>{ axios.get(`${url}/${id}`).then((res) =>{return res})};
+// export const updateUser = (id,user) =>{ axios.put(`${url}/${id}`,user).then((res) =>{return res})};
